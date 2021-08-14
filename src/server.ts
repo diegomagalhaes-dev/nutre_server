e07d8f301a0c97ddda19de40a8b7b8bd41a2cf95
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-08-13 21:06:23 -0300

    :hammer: initial development enviroument

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Workout hour!" });
-});
+app.use(routes);
 
 app.listen(3332, () => {
-  console.log("Server started on port 3332");
+  console.log('Server started on port 3332');
 });
