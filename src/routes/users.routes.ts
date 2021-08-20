2676fe61ed4fc400513f9fe215340003a4d6b9dd
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-08-20 17:17:10 -0300

    :hammer: refactoring routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,15 +9,15 @@ usersRouter.post('/', async (request, response) => {
 
     const createUser = new CreateUserService();
 
-    await createUser.execute({
+    const returnedUser = await createUser.execute({
       name,
       email,
       password,
     });
 
-    const returnedUser = { name, email };
+    const user = { ...returnedUser, password: undefined };
 
-    return response.json(returnedUser);
+    return response.json({ user });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
